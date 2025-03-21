8d7f60fca55ea64cadc86994e2301b04af303929
Rolando Cruz <rolandocrzprz@gmail.com>
2025-03-21 14:21:09 -0700

    Agregando manejo de errores en los controladores de usuarios

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -3,62 +3,94 @@ import { pool } from "../db.js";
 
 // Controlador para obtener todos los usuarios
 const getUsuarios = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM usuarios");
-  res.send(rows);
+  try {
+    const [rows] = await pool.query("SELECT * FROM usuarios");
+    res.send(rows);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Hubo un error al obtener todos los usuarios" });
+  }
 };
 
 // Controlador para obtener un usuario por su id
 const getUsuario = async (req, res) => {
-  const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (rows.length === 0) {
-    return res.status(404).json({ message: "Usuario no encontrado" });
+    if (rows.length === 0) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    res.send(rows[0]);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Hubo un error al obtener el usuario" });
   }
-
-  res.send(rows[0]);
 };
 
 // Controlador para crear un usuario
 const createUsuario = async (req, res) => {
-  const { nombre, domicilio, edad, sexo } = req.body;
-  const [rows] = await pool.query(
-    "INSERT INTO usuarios (nombre, domicilio, edad, sexo) VALUES (?, ?, ?, ?)",
-    [req.body.nombre, req.body.domicilio, req.body.edad, req.body.sexo]
-  );
-  res.send({ rows });
+  try {
+    const { nombre, domicilio, edad, sexo } = req.body;
+    const [rows] = await pool.query(
+      "INSERT INTO usuarios (nombre, domicilio, edad, sexo) VALUES (?, ?, ?, ?)",
+      [req.body.nombre, req.body.domicilio, req.body.edad, req.body.sexo]
+    );
+    res.send({ rows });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Hubo un error al crear el usuario" });
+  }
 };
 
 // Controlador para actualizar un usuario
 const updateUsuario = async (req, res) => {
-  const { nombre, domicilio, edad, sexo } = req.body;
-  const { id } = req.params;
-  const [result] = await pool.query(
-    "UPDATE usuarios SET nombre = IFNULL(?, nombre), domicilio = IFNULL(?, domicilio), edad = IFNULL(?, edad), sexo = IFNULL(?, sexo) WHERE id = ?",
-    [nombre, domicilio, edad, sexo, id]
-  );
+  try {
+    const { nombre, domicilio, edad, sexo } = req.body;
+    const { id } = req.params;
+    const [result] = await pool.query(
+      "UPDATE usuarios SET nombre = IFNULL(?, nombre), domicilio = IFNULL(?, domicilio), edad = IFNULL(?, edad), sexo = IFNULL(?, sexo) WHERE id = ?",
+      [nombre, domicilio, edad, sexo, id]
+    );
 
-  if (result.affectedRows === 0) {
-    return res.status(404).json({ message: "Usuario no encontrado" });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [
+      id,
+    ]);
+
+    res.send(rows[0]);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Hubo un error al actualizar el usuario" });
   }
-
-  const [rows] = await pool.query("SELECT * FROM usuarios WHERE id = ?", [id]);
-
-  res.send(rows[0]);
 };
 
 // Controlador para eliminar un usuario
 const deleteUsuario = async (req, res) => {
-  const [result] = await pool.query("DELETE FROM usuarios WHERE id = ?", [
-    req.params.id,
-  ]);
+  try {
+    const [result] = await pool.query("DELETE FROM usuarios WHERE id = ?", [
+      req.params.id,
+    ]);
 
-  if (result.affectedRows === 0) {
-    return res.status(404).json({ message: "Usuario no encontrado" });
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    res.sendStatus(204);
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Hubo un error al eliminiar el usuario" });
   }
-
-  res.sendStatus(204);
 };
 
 export { getUsuarios, getUsuario, createUsuario, updateUsuario, deleteUsuario };
