fab7b1e5788bd0b3f59c89c516e2329b1445638d
Rolando Cruz <rolandocrzprz@gmail.com>
2025-03-31 13:18:35 -0700

    datos de db actualizados

--- db/database.sql
+++ db/database.sql
@@ -1,19 +1,12 @@
-CREATE DATABASE usuarios;
-USE usuarios;
+CREATE DATABASE railway;
+USE railway;
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    nombre VARCHAR(100),
-    domicilio VARCHAR(100),
-    edad INT,
-    sexo VARCHAR(1)
+CREATE TABLE alumnos (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    matricula VARCHAR(10),
+    nombre VARCHAR(45),
+    domicilio VARCHAR(45),
+    fechanac DATE,
+    sexo VARCHAR(45),
+    status INT DEFAULT 0
 );
-
-DESCRIBE usuarios;
-
-
-INSERT INTO usuarios (nombre, domicilio, edad, sexo) VALUES 
-('Jesus', 'Calle 1', 20, 'M'),
-('Rolando', 'Calle 2', 21, 'M'),
-('Cruz', 'Calle 3', 22, 'M'),
-('Perez', 'Calle 4', 23, 'M');
